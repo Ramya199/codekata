a9f5b8ca2edd6c59ea158433c1f26ee4c9da333e
Ramya199 <ramyaharini1994@gmail.com>
2017-06-22 16:16:34 +0530

    Update fact2.py

--- fact2.py
+++ fact2.py
@@ -1,6 +1,6 @@
-num=int(raw_input("Enter a number"))
+number=int(raw_input("Enter a number"))
 n=1
-while num>0:
-	n=n*num
-	num=num-1
+while number>0:
+	n=n*number
+	number=number-1
 print "Factorial of the given number is ",n
